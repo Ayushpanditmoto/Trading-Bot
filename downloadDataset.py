4444f7fc9701bd6d0eeebe9c4b8965f6e77c3231
gratusrichard <gratusri@gmail.com>
2024-05-11 22:25:52 +0530

    minor comment

--- downloadDataset.py
+++ downloadDataset.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import os
 import opendatasets as od
-
-
-
-
 # Assign the Kaggle data set URL into variable
+
+
+##change this to the preferred dataset url.
+
 dataset = 'https://www.kaggle.com/datasets/gratusricharda/trading-bot-datasets'
-# Using opendatasets let's download the data sets
+
+
+
+
 od.download(dataset)
