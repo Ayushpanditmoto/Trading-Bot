fb3ba9d19e5e1859967e64be1420f7e6d187f8a3
Ayush Pandit <ayushpanditmoto@gmail.com>
2024-05-12 18:11:48 +0530

    feat: added customkinter ui

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # Code start here
+
+import customtkinter
+
+customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+app.geometry("400x240")
+
+def button_function():
+    print("button pressed")
+
+# Use CTkButton instead of tkinter Button
+button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
+button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+
+app.mainloop()
